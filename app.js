0d286e4f69535c5e1f15c44e2e7f62e5bb7cc7cb
flakyfilibuster <flakyfilibuster@gmail.com>
2015-07-24 17:50:24 +0200

    Add play-sound and randomize sound over 10 hits

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var play = require('play');
+var player = require('play-sound')(opts = {});
 var app = express();
 
 var intervalTime = 9000;
@@ -15,43 +15,43 @@ app.get('/', function (req, res) {
   hits.push(1);
   total.push(1);
 
-  console.log(hits.length);
   hl = hits.length;
 
   switch (true) {
     case (hl === 1):
-      play.sound('sounds/submarine.m4a');
-      if (!firstBlood) { firstBlood = true; play.sound('sounds/01.wav'); }
+      player.play('sounds/submarine.m4a');
+      if (!firstBlood) { firstBlood = true; player.play('sounds/01.wav'); }
       break;
     case (hl === 2):
-      play.sound('sounds/02.wav');
+      player.play('sounds/02.wav');
       break;
     case (hl === 3):
-      play.sound('sounds/03.wav');
+      player.play('sounds/03.wav');
       break;
     case (hl === 4):
-      play.sound('sounds/04.wav');
+      player.play('sounds/04.wav');
       break;
     case (hl === 5):
-      play.sound('sounds/05.wav');
+      player.play('sounds/05.wav');
       break;
     case (hl === 6):
-      play.sound('sounds/06.wav');
+      player.play('sounds/06.wav');
       break;
     case (hl === 7):
-      play.sound('sounds/07.wav');
+      player.play('sounds/07.wav');
       break;
     case (hl === 8):
-      play.sound('sounds/08.wav');
+      player.play('sounds/08.wav');
       break;
     case (hl === 9):
-      play.sound('sounds/09.wav');
+      player.play('sounds/09.wav');
       break;
     case (hl === 10):
-      play.sound('sounds/09.wav');
+      player.play('sounds/09.wav');
       break;
     case (hl > 10):
-      play.sound('sounds/111.wav');
+      var nr = [10, 11, 111][Math.round(Math.random()*2)];
+      player.play('sounds/' +nr+ '.wav');
       break;
     default:
       console.log('ehm, something went wrong');
