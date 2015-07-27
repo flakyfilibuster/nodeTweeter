c04b50f5cc39ce5154cd7756b481c777e0bcf3b5
flakyfilibuster <flakyfilibuster@gmail.com>
2015-07-27 17:31:06 +0200

    Add Date log and increase interval to 10sec

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var player = require('play-sound')(opts = {});
 var https = require('https');
 var cfg = require('./cfg.json');
-var intervalTime = 1000;
+var intervalTime = 10000;
 
 
 setInterval(function() {
+
+  var date = Date.now();
+
   var options = {
     hostname: cfg.hostName,
-    path: cfg.path + 'since=' + Date.now() + '&token=' + cfg.token,
+    path: cfg.path + 'since=' + date + '&token=' + cfg.token,
     method: 'GET'
   };
 
   https.get(options, function(res) {
+    console.log("Date: ", date);
     console.log("Got response: " + res.statusCode);
     res.on('data', function (chunk) {
       console.log('BODY: ' + chunk);
