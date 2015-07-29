10f1fb7f0502913432f477ce221c6c8c5d9d2587
flakyfilibuster <flakyfilibuster@gmail.com>
2015-07-29 10:17:38 +0200

    Change date settings to play nice with server

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var player = require('play-sound')(opts = {});
 var https = require('https');
 var cfg = require('./cfg.json');
-var intervalTime = 10000;
+var intervalTime = 5000;
 
-var date = Date.now();
+function utcDateString(d){
+  function pad(n){return (n<10 ? '0'+n : n); }
+  return d.getUTCFullYear()+'-'
+    + pad(d.getUTCMonth()+1)+'-'
+    + pad(d.getUTCDate())+'T'
+    + pad(d.getUTCHours())+':'
+    + pad(d.getUTCMinutes())+':'
+    + pad(d.getUTCSeconds())+'Z';
+}
+
+var date = new Date();
 
 setInterval(function() {
+
+  date = utcDateString(date);
 
   var options = {
     hostname: cfg.hostName,
@@ -14,13 +26,15 @@ setInterval(function() {
   };
 
   var req = https.get(options, function(res) {
+    console.log('pre date', date);
 
     // reset date with headers.date
-    date = Date.parse(res.headers.date);
+    date = new Date(Date.parse(res.headers.date));
+
     res.on('data', function (chunk) {
       var jsonRsp = JSON.parse(chunk);
 
-      console.log('Negotiation: %s ==> %s', new Date(date), jsonRsp.negotiations);
+      console.log('Negotiation: %s ==> %s', date, jsonRsp.negotiations);
 
       if(jsonRsp.negotiations) {
         player.play('sounds/submarine.m4a');
